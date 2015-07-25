a10d6ee93b25c869bcd815e82d939dda1d45b220
zhoulin <564339964@qq.com>
2015-07-25 16:29:48 +0800

    changed project pull

--- src/com/busycoder/servlet/HelloWorld.java
+++ src/com/busycoder/servlet/HelloWorld.java
@@ -9,16 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloWorld
- */
+
 @WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public HelloWorld() {
         super();
         // TODO Auto-generated constructor stub
